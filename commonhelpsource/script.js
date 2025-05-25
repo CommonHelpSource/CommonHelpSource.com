287943b79bfddf170d5520a42cffb64dc5d91040
CommonHelpSource <ccastro@commonhelpsource.com>
2025-05-25 10:08:49 -0400

    commit to changes

--- commonhelpsource/script.js
+++
@@ -1,6 +0,0 @@
-
-function startAssessment() {
-  const zip = document.getElementById('zipCode').value;
-  localStorage.setItem('user_zip', zip);
-  window.location.href = 'assessment_start.html';
-}
